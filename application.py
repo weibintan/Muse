e4d35d75a164d6f4dabad3b24aa971f04fce1320
Vincent <Vincent@Nicole.local>
2020-09-23 02:07:16 +0800

    ： enter the commit message for your changes. Lines starting test by vincent#

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'this is a python flask site'
+    return 'test'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
